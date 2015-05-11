38d4e820435f4bbebcc1cacaa0def8dd7cbc112a
fcharest <frederik.charest@radio-canada.ca>
2015-05-11 15:08:24 -0400

    initial commit

---
+++ src/tabs-ui.js
@@ -0,0 +1,36 @@
+define(['text!./tabs.html', 'knockout', 'jquery'],
+    function(template, ko, $) {
+        'use strict';
+
+        var TabsViewModel = function(settings, componentInfo) {
+            this.element = $(componentInfo.element);
+
+            this.element.attr('role', 'tabpanel');
+            this.element.on('click', 'tab-switcher a', onTabSwitcherClick);
+        };
+
+        TabsViewModel.prototype.dispose = function() {
+            this.element.off('click', 'tab-switcher a', onTabSwitcherClick);
+        };
+
+        function onTabSwitcherClick(event) {
+            $(event.delegateTarget).find('ul.nav.nav-tabs > li').each(function() {
+                var $li = $(this);
+
+                if ($li.children('a[href="' + event.target.hash + '"]').length) {
+                    $li.addClass('active');
+                } else if ($li.hasClass('active')) {
+                    $li.removeClass('active');
+                }
+            });
+        }
+
+        return {
+            viewModel: {
+                createViewModel: function(settings, componentInfo) {
+                    return new TabsViewModel(settings, componentInfo);
+                }
+            },
+            template: template
+        };
+    });
