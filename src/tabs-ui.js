283927cc1b06762a870f02165ae902d36aba5611
Maxime Forget-Séguin <maxime.forget-seguin@radio-canada.ca>
2016-05-24 13:23:25 -0400

    update to koco2

--- src/tabs-ui.js
+++ src/tabs-ui.js
@@ -1,29 +1,28 @@
-define(['knockout', 'jquery'],
-    function(ko, $) {
-        'use strict';
+import ko from 'knockout';
+import $ from 'jquery';
 
-        var TabsViewModel = function(settings, componentInfo) {
-            this.element = $(componentInfo.element);
 
-            this.element.attr('role', 'tabpanel');
-            this.element.on('click', 'tab-switcher a', onTabSwitcherClick);
-        };
+var TabsViewModel = function(settings, componentInfo) {
+    this.element = $(componentInfo.element);
 
-        TabsViewModel.prototype.dispose = function() {
-            this.element.off('click', 'tab-switcher a', onTabSwitcherClick);
-        };
+    this.element.attr('role', 'tabpanel');
+    this.element.on('click', 'tab-switcher a', onTabSwitcherClick);
+};
 
-        function onTabSwitcherClick(event) {
-            $(event.delegateTarget).find('ul.nav.nav-tabs > li').each(function() {
-                var $li = $(this);
+TabsViewModel.prototype.dispose = function() {
+    this.element.off('click', 'tab-switcher a', onTabSwitcherClick);
+};
 
-                if ($li.children('a[href="' + event.target.hash + '"]').length) {
-                    $li.addClass('active');
-                } else if ($li.hasClass('active')) {
-                    $li.removeClass('active');
-                }
-            });
+function onTabSwitcherClick(event) {
+    $(event.delegateTarget).find('ul.nav.nav-tabs > li').each(function() {
+        var $li = $(this);
+
+        if ($li.children('a[href="' + event.target.hash + '"]').length) {
+            $li.addClass('active');
+        } else if ($li.hasClass('active')) {
+            $li.removeClass('active');
         }
-
-        return TabsViewModel;
     });
+}
+
+export default TabsViewModel;
